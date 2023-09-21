718754a56df91f725e95ea1068630f2761e2d7ad
suryapsp <techie-human@protonmail.com>
2023-09-21 21:41:41 +0530

    basic layout done

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
   render() {
+    let {title, description, urlToImage, url} = this.props;
     return (
-      <div>NewsItem</div>
-    )
+      <div className="my-3">
+        <div className="card" style={{width: "18 rem"}}>
+          <img src={urlToImage} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">
+              {description}....
+            </p>
+            <a href={url} target="_blank" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
