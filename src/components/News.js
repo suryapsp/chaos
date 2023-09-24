565087d20e9fe87937a9cc5813ef172f05eb50f1
suryapsp <techie-human@protonmail.com>
2023-09-24 20:29:42 +0530

    added date, time and author

--- src/components/News.js
+++ src/components/News.js
@@ -83,6 +83,8 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
+                  author={element.author}
+                  date={element.publishedAt}
                   title={element.title ? element.title.slice(0, 45) : ""}
                   description={
                     element.description ? element.description.slice(0, 88) : ""
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
-    let {title, description, urlToImage, url} = this.props;
+    let { title, description, urlToImage, url, author, date } = this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={!urlToImage?"https://static.vecteezy.com/system/resources/thumbnails/004/216/831/small/3d-world-news-background-loop-free-video.jpg":urlToImage} className="card-img-top" alt="..." />
+          <img
+            src={
+              !urlToImage
+                ? "https://static.vecteezy.com/system/resources/thumbnails/004/216/831/small/3d-world-news-background-loop-free-video.jpg"
+                : urlToImage
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}....</h5>
             <p className="card-text">
-              {description}....
+              <small className="text-muted">
+                On {new Date(date).toUTCString()} by {author ? author : "unknown author"}
+              </small>
             </p>
-            <a rel="noreferrer" href={url} target="_blank" className="btn btn-dark">
+            <p className="card-text">{description}....</p>
+            <a
+              rel="noreferrer"
+              href={url}
+              target="_blank"
+              className="btn btn-dark"
+            >
               Read More
             </a>
           </div>
